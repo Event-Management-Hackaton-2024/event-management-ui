920e9611e81cb9e4c2d60fc532d8e3005ef383d4
ignatIgnatov <ignatov_rs@abv.bg>
2024-04-20 21:22:55 +0300

    fix bug

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -10,8 +10,10 @@ export const loginAPICall = (email, password) =>
   axios.post(AUTH_REST_API_BASE_URL + "/login", { email, password });
 
 export const storeToken = (token) => localStorage.setItem("token", token);
+export const storeEmail = (email) => localStorage.setItem("email", email);
 
 export const getToken = () => localStorage.getItem("token");
+export const getEmail = () => localStorage.getItem("email");
 
 export const saveLoggedInUser = (email, role) => {
   sessionStorage.setItem("authenticatedUser", email);
@@ -27,17 +29,17 @@ export const isUserLoggedIn = () => {
     return true;
   }
 };
-export const getUserInfo = async (token) => {
+export const getUserInfo = async (token, email) => {
   try {
-    const response = await axios.get(`${BASE_URL}/users/${token}`,
+    const response = await axios.get(`${BASE_URL}/users/${email}`,
       {
         headers: {
           Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
         },
       });
-    
+
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch user information");
@@ -55,7 +57,7 @@ export const logout = () => {
 
 export const isAdminUser = async () => {
   try {
-    const user = await getUserInfo(getToken());
+    const user = await getUserInfo(getToken(), getEmail());
 
     console.log(user);
 
